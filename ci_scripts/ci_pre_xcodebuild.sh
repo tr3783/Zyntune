b4d4774836029406914a50dae72f7e335cccd81f
Topher Ruggiero <Rug6@aol.com>
2026-06-14 22:09:49 -0400

    Fix Xcode Cloud: ensure CocoaPods installed before pod install

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+set -e
+
+# Install CocoaPods if not available
+if ! command -v pod &> /dev/null; then
+  gem install cocoapods
+fi
+
 cd $CI_PRIMARY_REPOSITORY_PATH/ios
-pod install
+pod install --repo-update
